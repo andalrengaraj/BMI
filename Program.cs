5f9ff1dce7475c340bfa69d42d9c2ed1b89b9d90
andalrengaraj <74759488+andalrengaraj@users.noreply.github.com>
2020-12-01 21:49:10 +0530

    height  of the person code

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace height1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your height in cms");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+             double heightft = height / 30.48;
+
+             double heightininches = height / 2.54;
+
+
+             Console.WriteLine("Your Height in feet:" +heightft);
+             Console.WriteLine("Your Height in inches:" +heightininches);
+
+        }
+    }
+}
